d79eda84aafb74cfaa85375a4fc7460fffa805f7
LOctavio <luis_ram1997@hotmail.com>
2022-03-19 12:17:59 -0600

    Add contact section

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,12 +2,14 @@ import Introduction from './sections/Introduction';
 import './Main.css';
 import Portfolio from './sections/Portfolio/Portfolio';
 import About from './sections/About';
+import Contact from './sections/Contact';
 
 const Main = () => (
   <>
     <Introduction />
     <Portfolio />
     <About />
+    <Contact />
   </>
 );
 
---
+++ src/main/sections/Contact.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+
+const Contact = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_x3b6hwb',
+      'template_qljctsc',
+      toSend,
+      'nNfPPqTKlASgD5TvX',
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <span>Interested in collaborating?</span>
+      <h1>Get started</h1>
+      <span>You can use this form to send me an email so that we can get in touch</span>
+      <div>
+        <form onSubmit={onSubmit}>
+          <div className="mt-3 mb-3">
+            <input
+              className="form-control"
+              id="name"
+              type="text"
+              name="from_name"
+              placeholder="Your name"
+              value={toSend.from_name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              id="message"
+              style={{ height: '100px' }}
+              type="text"
+              name="message"
+              placeholder="Your message"
+              value={toSend.message}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              id="email"
+              type="text"
+              name="reply_to"
+              placeholder="Your email"
+              value={toSend.reply_to}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-dark">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
